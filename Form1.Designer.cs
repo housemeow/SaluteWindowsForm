fb5ef66590caece47237afd3e1756e0488bdf707
KellyYang <housemeow@gmail.com>
2014-12-29 15:35:40 +0800

    upload code

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Salute
+{
+    partial class SaluteForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this._closeButton = new System.Windows.Forms.Button();
+            this._saluteTimer = new System.Windows.Forms.Timer(this.components);
+            this._comportNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this._connectButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this._arduinoTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this._comportNumericUpDown)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _closeButton
+            // 
+            this._closeButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._closeButton.Location = new System.Drawing.Point(259, 3);
+            this._closeButton.Name = "_closeButton";
+            this._closeButton.Size = new System.Drawing.Size(170, 23);
+            this._closeButton.TabIndex = 0;
+            this._closeButton.Text = "Close";
+            this._closeButton.UseVisualStyleBackColor = true;
+            this._closeButton.Click += new System.EventHandler(this.ClickCloseButton);
+            // 
+            // _saluteTimer
+            // 
+            this._saluteTimer.Tick += new System.EventHandler(this.TickSaluteTimer);
+            // 
+            // _comportNumericUpDown
+            // 
+            this._comportNumericUpDown.Location = new System.Drawing.Point(3, 3);
+            this._comportNumericUpDown.Name = "_comportNumericUpDown";
+            this._comportNumericUpDown.Size = new System.Drawing.Size(67, 22);
+            this._comportNumericUpDown.TabIndex = 1;
+            this._comportNumericUpDown.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // _connectButton
+            // 
+            this._connectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._connectButton.Location = new System.Drawing.Point(83, 3);
+            this._connectButton.Name = "_connectButton";
+            this._connectButton.Size = new System.Drawing.Size(170, 23);
+            this._connectButton.TabIndex = 2;
+            this._connectButton.Text = "Connect";
+            this._connectButton.UseVisualStyleBackColor = true;
+            this._connectButton.Click += new System.EventHandler(this.ClickConnect);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this._closeButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this._comportNumericUpDown, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this._connectButton, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(432, 29);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // _arduinoTimer
+            // 
+            this._arduinoTimer.Enabled = true;
+            this._arduinoTimer.Tick += new System.EventHandler(this.HandleArduinoThreadSafe);
+            // 
+            // SaluteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 260);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "SaluteForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoadForm);
+            ((System.ComponentModel.ISupportInitialize)(this._comportNumericUpDown)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _closeButton;
+        private System.Windows.Forms.Timer _saluteTimer;
+        private System.Windows.Forms.NumericUpDown _comportNumericUpDown;
+        private System.Windows.Forms.Button _connectButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer _arduinoTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Salute
+{
+    public partial class SaluteForm : Form
+    {
+        enum SaluteState
+        {
+            WaitSalute,
+            Salute,
+            WaitLeave
+        }
+
+        SaluteState _state = SaluteState.WaitSalute;
+
+        private Bitmap[] bitmap = new Bitmap[7];
+        int _index;
+        SerialPort _arduino;
+        private double distance = 1000;
+        private const double DISTANCE = 175;
+        int counter;
+        private int LEAVE_COUNT = 3;
+        private int WAIT_SALUTE_COUNT = 3;
+
+        public SaluteForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadForm(object sender, EventArgs e)
+        {
+            bitmap[0] = new Bitmap(Properties.Resources.frame_000);
+            bitmap[1] = new Bitmap(Properties.Resources.frame_001);
+            bitmap[2] = new Bitmap(Properties.Resources.frame_002);
+            bitmap[3] = new Bitmap(Properties.Resources.frame_003);
+            bitmap[4] = new Bitmap(Properties.Resources.frame_004);
+            bitmap[5] = new Bitmap(Properties.Resources.frame_005);
+            bitmap[6] = new Bitmap(Properties.Resources.frame_006);
+            Size = new Size(bitmap[0].Width, bitmap[0].Height);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            BackgroundImage = bitmap[0];
+            DoubleBuffered = true;
+            counter = 0;
+        }
+
+        private void InitArduino()
+        {
+            string comPortName = "COM" + _comportNumericUpDown.Value;
+            try
+            {
+                _arduino = new SerialPort(comPortName, 9600, Parity.None, 8, StopBits.One);
+                if ((_arduino != null) && (!_arduino.IsOpen))
+                {
+                    _arduino.Open();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                _arduino = null;
+            }
+        }
+
+        private void EndSulate()
+        {
+            _saluteTimer.Enabled = false;
+            _index = 0;
+            BackgroundImage = bitmap[_index];
+        }
+
+        private void ClickCloseButton(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void TickSaluteTimer(object sender, EventArgs e)
+        {
+            BackgroundImage = bitmap[_index++];
+            if (_index == 7)
+            {
+                _saluteTimer.Enabled = false;
+                //BackgroundImage = bitmap[0];
+                _index = 0;
+                _state = SaluteState.WaitLeave;
+            }
+        }
+
+        private void ClickConnect(object sender, EventArgs e)
+        {
+            InitArduino();
+            if (_arduino != null)
+            {
+                _arduino.DataReceived += new SerialDataReceivedEventHandler(HandleArduinoData);
+                _connectButton.Enabled = false;
+                _comportNumericUpDown.Enabled = false;
+            }
+        }
+
+        private void HandleArduinoData(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            try
+            {
+                distance = double.Parse(indata);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void HandleArduinoThreadSafe(object sender, EventArgs e)
+        {
+            ProcessState(distance);
+        }
+
+        private void ProcessState(double distance)
+        {
+            Console.WriteLine(Enum.GetName(typeof(SaluteState), _state));
+            Console.WriteLine(counter);
+            switch (_state)
+            {
+                case SaluteState.WaitSalute:
+                    if (distance < DISTANCE)
+                        counter++;
+                    else
+                        //counter = counter - 1 > 0 ? counter - 1 : 0;
+                        counter = 0;
+                    if (counter > WAIT_SALUTE_COUNT)
+                    {
+                        _index = 0;
+                        _saluteTimer.Enabled = true;
+                        _state = SaluteState.Salute;
+                        counter = 0;
+                    }
+                    break;
+                case SaluteState.Salute:
+                    break;
+                case SaluteState.WaitLeave:
+                    if (distance >= DISTANCE)
+                        counter++;
+                    else
+                        //counter = counter - 1 > 0 ? counter - 1 : 0;
+                        counter = 0;
+                    if (counter > LEAVE_COUNT)
+                    {
+                        _state = SaluteState.WaitSalute;
+                        counter = 0;
+                        BackgroundImage = bitmap[0];
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
